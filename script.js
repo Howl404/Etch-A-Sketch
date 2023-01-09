eda3a8b7a1a61071e51612ce7e24b125bf3e062c
Howl404 <114860794+Howl404@users.noreply.github.com>
2023-01-09 16:58:50 +0200

    Done

--- script.js
+++ script.js
@@ -1,16 +1,49 @@
-for (i = 0; i < 256; i++) {
+for (i = 0; i < 4096; i++) {
   document
     .querySelector(".container")
     .appendChild(document.createElement("div"));
 }
 
-const divs = document.querySelectorAll("div.container > div");
+let divs = document.querySelectorAll("div.container > div");
 
 divs.forEach(function (currentDiv) {
   currentDiv.addEventListener("mouseover", function () {
     currentDiv.classList.add("hover");
-    setTimeout(() => {
-      currentDiv.classList.remove("hover");
-    }, 500);
   });
 });
+
+const container = document.querySelector(".container");
+
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", function () {
+  let numberOfDivs = prompt("Number of divs");
+  while (numberOfDivs > 100) {
+    alert("Maximum allowed number of divs is 100, please enter valid number.");
+    numberOfDivs = prompt("Number of divs");
+  }
+  createDivs(numberOfDivs);
+});
+
+function createDivs(amount) {
+  a = amount * amount;
+  function removeAllChildNodes(parent) {
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild);
+    }
+  }
+  removeAllChildNodes(container);
+  for (i = 0; i < a; i++) {
+    document
+      .querySelector(".container")
+      .appendChild(document.createElement("div"));
+  }
+  container.style.gridTemplateRows = `repeat(${amount}, 1fr)`;
+  container.style.gridTemplateColumns = `repeat(${amount}, 1fr)`;
+  divs = document.querySelectorAll("div.container > div");
+  divs.forEach(function (currentDiv) {
+    currentDiv.addEventListener("mouseover", function () {
+      currentDiv.classList.add("hover");
+    });
+  });
+}
